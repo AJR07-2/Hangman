9c43fc701a3cb6f1427706d5886d584327c1aefa
Ang Jun Ray <angjunray@live.com>
2020-11-23 20:02:50 +0800

    Update tic tac toe ai.py

--- tic tac toe ai.py
+++ tic tac toe ai.py
@@ -4,9 +4,9 @@ print("This tic tac toe AI Machine has been created by Ang Jun Ray. 23 Nov 2020"
 print("*********TO REQUEST MORE FEATURES/RSPORT A BUG, PLEASE LEAVE A NOTE ON GITHUB. *******")
 print("To play with the program, please input the place you would want to place your X or 0")
 print("It is defined by (row character)(column number)")
+time.sleep(5)
 #inputs
 while True:
-    try:
     a = input("Would you like to go first? (Yes/No)")
     if a == "Yes" or a == "No":
         break
