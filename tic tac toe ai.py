a28df2fd171fd5ffdc0b816cd1a15a507cf0e244
Ang Jun Ray <angjunray@live.com>
2020-11-23 20:00:55 +0800

    Basic front section done
    Did all the front work, printing the stuff, the first inouts...

--- tic tac toe ai.py
+++ tic tac toe ai.py
@@ -1,6 +1,15 @@
+import time
+# Instructions for player
+print("This tic tac toe AI Machine has been created by Ang Jun Ray. 23 Nov 2020")
+print("*********TO REQUEST MORE FEATURES/RSPORT A BUG, PLEASE LEAVE A NOTE ON GITHUB. *******")
+print("To play with the program, please input the place you would want to place your X or 0")
+print("It is defined by (row character)(column number)")
+#inputs
 while True:
     try:
-        a = input("Would you like to begin first(Yes/No)")
-
-    except:
-        print("Error, input is not Yes/No")
+    a = input("Would you like to go first? (Yes/No)")
+    if a == "Yes" or a == "No":
+        break
+    else:
+        print("Capitalization matters, please input exactly Yes or No")
+if a  == "Yes":
