72ca66d582fa5d96c547efa9c3cf9351fce45293
Ang Jun Ray <angjunray@live.com>
2020-11-24 15:48:20 +0800

    conversion to c++

--- tic tac toe/tic tac toe/main.cpp
+++ tic tac toe/tic tac toe/main.cpp
@@ -1,15 +1,19 @@
-import time
-# Instructions for player
-print("This tic tac toe AI Machine has been created by AJR07, 23 Nov 2020")
-print("*********TO REQUEST MORE FEATURES/RSPORT A BUG, PLEASE LEAVE A NOTE ON GITHUB. *******")
-print("To play with the program, please input the place you would want to place your X or 0")
-print("It is defined by (row character)(column number)")
-time.sleep(5)
-#inputs
-while True:
-    a = input("Would you like to go first? (Yes/No)")
-    if a == "Yes" or a == "No":
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    //MARK: Instructions for player
+    cout << "This tic tac toe AI Machine has been created by AJR07, 23 Nov 2020" << endl;
+    cout << "*********TO REQUEST MORE FEATURES/RSPORT A BUG, PLEASE LEAVE A NOTE ON GITHUB. *******" << endl;
+    cout<< "To play with the program, please input the place you would want to place your X or 0" << endl;
+    cout << "It is defined by (row character)(column number)" << endl;
+    //MARK: Inputs
+    while (true){
+        cout << "Would you like to go first"
+        if a == "Yes" or a == "No":
         break
-    else:
+        else:
         print("Capitalization matters, please input exactly Yes or No")
-if a  == "Yes":
+        if a  == "Yes":
+    }
+}
+
