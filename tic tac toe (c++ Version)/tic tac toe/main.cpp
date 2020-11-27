793ee4020e333652500a7cd9045f2352fb435244
Ang Jun Ray <angjunray@live.com>
2020-11-27 20:41:03 +0800

    More changes from python to c++

--- tic tac toe (c++ Version)/tic tac toe/main.cpp
+++ tic tac toe (c++ Version)/tic tac toe/main.cpp
@@ -35,13 +35,13 @@ bool check_score(){
             abool = false;
             return true;
         }
-
+        
         else if (board[winning_lines[i][0]] == false and board[winning_lines[i][1]] == false and board[winning_lines[i][2]] == false){
             cout << "You won!" << endl;
             abool = false;
             return true;
         }
-
+        
         if (abool == true){
             bool bbool = true;
             for (int i = 0; i < 9; i++){
@@ -54,128 +54,131 @@ bool check_score(){
                 return true;
             }
         }
-
-
+    }
+    return false;
 }
 
-def check_win():
-for i in range(8):
-if board[winning_lines[i][0]] == True and board[winning_lines[i][1]] == True and board[
-                                                                                       winning_lines[i][2]] == None:
-return winning_lines[i][2]
-elif board[winning_lines[i][0]] == True and board[winning_lines[i][2]] == True and board[
-                                                                                         winning_lines[i][1]] == None:
-return winning_lines[i][1]
-elif board[winning_lines[i][1]] == True and board[winning_lines[i][2]] == True and board[
-                                                                                         winning_lines[i][0]] == None:
-return winning_lines[i][0]
-
-
+string check_win(){
+    for (int i = 0; i < 8; i++){
+        if (board[winning_lines[i][0]] == 1 and board[winning_lines[i][1]] == 1 and board[winning_lines[i][2]] == 0){
+            return winning_lines[i][2];
+        }
+        else if (board[winning_lines[i][0]] == 1 and board[winning_lines[i][2]] == 1 and board[winning_lines[i][1]] == 0){
+            return winning_lines[i][1];
+        }
+        else if (board[winning_lines[i][1]] == 1 and board[winning_lines[i][2]] == 1 and board[winning_lines[i][0]] == 0){
+            return winning_lines[i][0];
+        }
+    }
+    return "0";
+}
+    
+    
 def check_double_attack():
-for a in available_moves:
-pseudo_board = copy.deepcopy(board)
-pseudo_board[a] = True
-counter = 0
-for i in range(8):
-if pseudo_board[winning_lines[i][0]] == True and pseudo_board[winning_lines[i][1]] == True and \
-pseudo_board[winning_lines[i][2]] == None:
-counter += 1
-elif pseudo_board[winning_lines[i][0]] == True and pseudo_board[winning_lines[i][2]] == True and \
-pseudo_board[winning_lines[i][1]] == None:
-counter += 1
-elif pseudo_board[winning_lines[i][1]] == True and pseudo_board[winning_lines[i][2]] == True and \
-pseudo_board[winning_lines[i][0]] == None:
-counter += 1
-if counter >= 2:
-return a
-
-
-def check_threat():
-for i in range(8):
-if board[winning_lines[i][0]] == False and board[winning_lines[i][1]] == False and board[
-                                                                                         winning_lines[i][2]] == None:
-return winning_lines[i][2]
-elif board[winning_lines[i][0]] == False and board[winning_lines[i][2]] == False and board[
-                                                                                           winning_lines[i][1]] == None:
-return winning_lines[i][1]
-elif board[winning_lines[i][1]] == False and board[winning_lines[i][2]] == False and board[
-                                                                                           winning_lines[i][0]] == None:
-return winning_lines[i][0]
-
-
-
-int main(){
-// ------------------------------------------------------------------
-// INSTRUCTIONS FOR PLAYER-------------------------------------------
-// ------------------------------------------------------------------
-    
-    cout << " This tic tac toe AI Machine has been created by AJR07, Collaborated by YNWAPythoner. 23 Nov 2020" << '\n' << "*********TO REQUEST MORE FEATURES/REPORT A BUG, PLEASE LEAVE A NOTE ON GITHUB. *******" << '\n' << "To play with the program, please input the place you would want to place your X" << '\n';
-    usleep(1000000);
-    cout <<  "It is defined by (row character)(column number)" << '\n' << "This is the board:" << '\n' << "1 2 3" << '\n' << "4 5 6" << '\n' << "7 8 9" << '\n';
-    usleep(1000000);
-    cout <<  "The indexes are what you are supposed to enter to select a move later on" << '\n' << "The player would be X all the time, whether he moves first or not. (Could be changed in the future)" << '\n';
-    usleep(5000000);
-
-// ------------------------------------------------------------------
-// INPUTS------------------------------------------------------------
-// ------------------------------------------------------------------
-    
-    while True:
-        try:
-        choice = int(input("Do you want to go first or second? (1 for first and 2 for second): "))
-        except:
-        print("That input is invalid, please retry")
-        continue
-        if choice == 1 or choice == 2:
-            break
-            else:
+    for a in available_moves:
+        pseudo_board = copy.deepcopy(board)
+        pseudo_board[a] = True
+        counter = 0
+        for i in range(8):
+        if pseudo_board[winning_lines[i][0]] == True and pseudo_board[winning_lines[i][1]] == True and \
+        pseudo_board[winning_lines[i][2]] == None:
+        counter += 1
+        elif pseudo_board[winning_lines[i][0]] == True and pseudo_board[winning_lines[i][2]] == True and \
+        pseudo_board[winning_lines[i][1]] == None:
+        counter += 1
+        elif pseudo_board[winning_lines[i][1]] == True and pseudo_board[winning_lines[i][2]] == True and \
+        pseudo_board[winning_lines[i][0]] == None:
+        counter += 1
+        if counter >= 2:
+        return a
+        
+        
+        def check_threat():
+        for i in range(8):
+        if board[winning_lines[i][0]] == False and board[winning_lines[i][1]] == False and board[
+                                                                                                 winning_lines[i][2]] == None:
+        return winning_lines[i][2]
+        elif board[winning_lines[i][0]] == False and board[winning_lines[i][2]] == False and board[
+                                                                                                   winning_lines[i][1]] == None:
+        return winning_lines[i][1]
+        elif board[winning_lines[i][1]] == False and board[winning_lines[i][2]] == False and board[
+                                                                                                   winning_lines[i][0]] == None:
+        return winning_lines[i][0]
+        
+        
+        
+        int main(){
+            // ------------------------------------------------------------------
+            // INSTRUCTIONS FOR PLAYER-------------------------------------------
+            // ------------------------------------------------------------------
+            
+            cout << " This tic tac toe AI Machine has been created by AJR07, Collaborated by YNWAPythoner. 23 Nov 2020" << '\n' << "*********TO REQUEST MORE FEATURES/REPORT A BUG, PLEASE LEAVE A NOTE ON GITHUB. *******" << '\n' << "To play with the program, please input the place you would want to place your X" << '\n';
+            usleep(1000000);
+            cout <<  "It is defined by (row character)(column number)" << '\n' << "This is the board:" << '\n' << "1 2 3" << '\n' << "4 5 6" << '\n' << "7 8 9" << '\n';
+            usleep(1000000);
+            cout <<  "The indexes are what you are supposed to enter to select a move later on" << '\n' << "The player would be X all the time, whether he moves first or not. (Could be changed in the future)" << '\n';
+            usleep(5000000);
+            
+            // ------------------------------------------------------------------
+            // INPUTS------------------------------------------------------------
+            // ------------------------------------------------------------------
+            
+            while True:
+                try:
+                choice = int(input("Do you want to go first or second? (1 for first and 2 for second): "))
+                except:
                 print("That input is invalid, please retry")
-                time.sleep(2)
-                
-                if choice == 2:
-                    board["5"] = True
-                    print("Computer played: 5")
-                    available_moves.pop(available_moves.index("5"))
-                    booly = True
-                    while True:
-                        display_board()
-                        selection = input("Enter your choice: ")
-                        x = selection
-                        board[x] = False
-                        available_moves.pop(available_moves.index(x))
-                        if check_score():
-                            break
-                            else:
-                                if check_win() is not None:
-                                    x = check_win()
-                                    board[x] = True
-                                    print(x)
-                                    available_moves.pop(available_moves.index(x))
-                                    check_score()
+                continue
+                if choice == 1 or choice == 2:
+                    break
+                    else:
+                        print("That input is invalid, please retry")
+                        time.sleep(2)
+                        
+                        if choice == 2:
+                            board["5"] = True
+                            print("Computer played: 5")
+                            available_moves.pop(available_moves.index("5"))
+                            booly = True
+                            while True:
+                                display_board()
+                                selection = input("Enter your choice: ")
+                                x = selection
+                                board[x] = False
+                                available_moves.pop(available_moves.index(x))
+                                if check_score():
                                     break
                                     else:
-                                        if check_threat() is not None:
-                                            x = check_threat()
+                                        if check_win() is not None:
+                                            x = check_win()
                                             board[x] = True
-                                            print("Computer played: " + x)
+                                            print(x)
                                             available_moves.pop(available_moves.index(x))
+                                            check_score()
+                                            break
                                             else:
-                                                if check_double_attack() is not None:
-                                                    x = check_double_attack()
+                                                if check_threat() is not None:
+                                                    x = check_threat()
                                                     board[x] = True
                                                     print("Computer played: " + x)
                                                     available_moves.pop(available_moves.index(x))
                                                     else:
-                                                        list1 = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-                                                        while True:
-                                                            x = random.choice(list1)
-                                                            if x in available_moves:
-                                                                break
-                                                                else:
-                                                                    list1.pop(list1.index(x))
-                                                                    board[x] = True
-                                                                    print("Computer played: " + x)
-                                                                    available_moves.pop(available_moves.index(x))
-                                                                    time.sleep(3)
-                                                                    }
+                                                        if check_double_attack() is not None:
+                                                            x = check_double_attack()
+                                                            board[x] = True
+                                                            print("Computer played: " + x)
+                                                            available_moves.pop(available_moves.index(x))
+                                                            else:
+                                                                list1 = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+                                                                while True:
+                                                                    x = random.choice(list1)
+                                                                    if x in available_moves:
+                                                                        break
+                                                                        else:
+                                                                            list1.pop(list1.index(x))
+                                                                            board[x] = True
+                                                                            print("Computer played: " + x)
+                                                                            available_moves.pop(available_moves.index(x))
+                                                                            time.sleep(3)
+                                                                            }
 }
