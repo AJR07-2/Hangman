974849593a731b5c71766cd4154be6ee680b2dd1
Ang Jun Ray <angjunray@live.com>
2020-12-25 22:12:07 +0800

    Update Hangman(Python Version).py

--- Hangman/Hangman(Python Version).py
+++ Hangman/Hangman(Python Version).py
@@ -1,5 +1,11 @@
 import time
+import bs4
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+import time
+#--------------------
 #introduction
+#--------------------
 intro = """
 Hello there, this is a Hangman Game made by AJR, Dec 23 2020
 More improvements will be made in the future, feel free to open an issue if u have any suggestions/There is any bugs!
@@ -12,8 +18,10 @@ Thanks and enjoy! :D
 """
 print(intro)
 time.sleep(5)
-
+#--------------------
 #functions
+#--------------------
+
 def error(input, requirements):
   print("Ur input: " + input + " is Invalid. Please input acccording to the requirements of(" + requirements + ") instead.")
 
@@ -23,16 +31,48 @@ words = open("/Users/angjunray/Desktop/Coding/General/Projects/Hangman/words.txt
 for topic in words:
   myList.append(topic)
 
+#--------------------
 #topic choice
+#--------------------
+
 while True:
   topic = input("What topic do you want to choose? Type ? for list of topics!")
   if topic == "?":
     print("----------LIST OF TOPICS AVALIABLE------------")
     for word in myList:
-      print(word)
+      print("'" + word + "'")
     
     print("------------------END OF LIST-----------------")
   elif topic in myList:
     break
   else:
     error(topic, "Input must be in the list of topics. input ? for list!")
+
+#--------------------
+#Web Scrape
+#--------------------
+
+
+
+my_url = "https://www.enchantedlearning.com/wordlist/" + topic + ".shtml"
+uClient = uReq(my_url)
+page_html =  uClient.read()
+uClient.close()
+
+#html parsing
+page=soup(page_html, "html.parser")
+container = page.findAll("div", {"class":"worldlist-section wordlist"})
+print(str(container))
+#time 
+startime = time.time()
+
+
+
+
+
+
+
+#check time
+endtime = time.time()
+totaltime = startime - endtime
+print("You took " + str(totaltime) + " to complete the Hangman word!")
