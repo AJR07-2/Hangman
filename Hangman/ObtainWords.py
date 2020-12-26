514b5bf3bc1410c1ab498c9b14e74b0d7af7e5e8
Ang Jun Ray <angjunray@live.com>
2020-12-26 16:52:12 +0800

    Major edits

--- Hangman/ObtainWords.py
+++ Hangman/ObtainWords.py
@@ -2,15 +2,27 @@ import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
+#--------FIRST PARSE OF LINKS----------
 my_url = "https://www.enchantedlearning.com/wordlist"
 uClient = uReq(my_url)
 page_html =  uClient.read()
 uClient.close()
 
-print("")
-print("")
 page=soup(page_html, "html.parser")
 
 table = page.select("body > p:nth-child(13) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1)")
 tables = page.find_all("td")
-print(tables)
+
+counter = 12
+links = []
+topic = []
+
+while counter <= 15:
+    container = tables[counter].find_all("a")
+    for label in container:
+        links.append(label.get('href'))
+        topic.append(label.text)
+    counter += 1
+
+
+
